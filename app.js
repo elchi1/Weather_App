a2a9901e57b947387639a72891b531ddce91719a
elchi1 <77587884+elchi1@users.noreply.github.com>
2021-03-07 22:38:37 +0600

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.post("/",function(req,res){
 
   const query = req.body.cityName; // клиент пишет свой город
   const apiId = ""; // наша апишка Чтобы все сработало добавьте свой API(зарегистрируйтесь на сайте open weather и все, вам выдадут личный апи)
-  const units = "metric"; // мы хотим чтобы нащы градусы показывали в метрической системе(Система Си)
+  const units = "metric"; // мы хотим чтобы наши градусы показывали в метрической системе(Система Си)
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units +"&appid=" + apiId;
   https.get(url,function(response){
     console.log(response.statusCode); // Здесь нам выдадут статус нашего response (200 - OK)
@@ -33,6 +33,8 @@ app.post("/",function(req,res){
       res.write("<p>Description is "+ description+"</p>"); // Выводим описание на странице сайта
       res.write("<h1>The temperature in " + query + " is "+ temp + " degree Celcuis </h1>"); //Выводим температуру на странице сайта
       res.write("<img src="+iconImageUrl+">"); // Выводим картинку(иконку) на странице сайта
+      // поскольку мы можем отправить только один раз то есть res.send мы можем использовать лишь раз в внутри этой функции, то мы все файлы 
+      // которые хотели бы выложить должны написать командой .write как показано выше
       res.send();
     });
   });
